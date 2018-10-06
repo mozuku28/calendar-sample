69d604f1729b71f74a9bebb59e5a21c1a80bc75a
mozukun28 <mozukun28@gmail.com>
2018-10-06 16:37:00 +0000

    Completion

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # README
 
 * カレンダーを表示して、予定を入れたい。
+* 参考サイト：https://qiita.com/gotchane/items/dd2860ed1f33bd635914
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :meetings do
+    collection do
+      post :update_week
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
